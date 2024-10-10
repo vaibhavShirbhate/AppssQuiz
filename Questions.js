c307576c42890a4a791b50b8367f4e5e9c7f4596
vaibhavShirbhate <99351958+vaibhavShirbhate@users.noreply.github.com>
2024-10-10 21:16:00 +0530

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. HyperText Markup Language",
+        options: [
+            "A. HyperText Machine Language",
+            "B. HyperText and links Markup Language",
+            "C. HyperText Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stands for?",
+        answer: "C. Cascading style sheets",
+        options: [
+            "A. Cascade style sheets",
+            "B. Color and style sheets",
+            "C. Cascading style sheets",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stands for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Pretext Hypertext Preprocessor",
+            "C. Personal Home Processor",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stands for?",
+        answer: "A. Structured Query Language",
+        options: [
+            "A. Structured Query Language",
+            "B. Sequential Query Language",
+            "C. Structured Question Language",
+            "D. Sequential Question Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the full form of DBMS?",
+        answer: "B. Database Management System",
+        options: [
+            "A. Data of Binary Management System",
+            "B. Database Management System",
+            "C. Database Management Service",
+            "D. Data Backup Management System"
+        ]
+    }
+];
